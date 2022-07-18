0bdd97a772871202533eeb869589e0115d5fdbb8
Eric <eli7@hawk.iit.edu>
2022-07-18 14:09:45 -0500

    fix: enable grid resizing control

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,12 +1,17 @@
-const renderGrid = () => {
-  const gridHeight = 16;
-  const gridWidth = 16;
+const renderGrid = (gridSize) => {
+  const previousGrid = document.getElementById("grid-container");
+  previousGrid.remove();
+  const etchASketchContainer = document.getElementById("etch-a-sketch");
+  const knobContainer = document.getElementById("knob-container");
+  const newGridContainer = document.createElement("div");
+  newGridContainer.id = "grid-container";
 
-  for (let i = 0; i < gridHeight; i++) {
+  etchASketchContainer.insertBefore(newGridContainer, knobContainer);
+  for (let i = 0; i < gridSize; i++) {
     let gridRow = document.createElement("div");
     gridRow.className = "grid-row";
 
-    for (let i = 0; i < gridWidth; i++) {
+    for (let i = 0; i < gridSize; i++) {
       let gridCell = document.createElement("div");
       gridCell.className = "grid-cell";
       gridRow.appendChild(gridCell);
@@ -22,25 +27,27 @@ const renderCellHoverEffect = () => {
     cell.addEventListener("mouseover", (event) => {
       cell.style.backgroundColor = "black";
     });
-    // cell.addEventListener("mouseout", (event) => {
-    //   cell.style.backgroundColor = "white";
-    // });
   });
 };
 
 const clearGrid = () => {
   let clearButton = document.getElementById("clear-button");
   let gridCells = document.querySelectorAll(".grid-cell");
-
   clearButton.addEventListener("click", (event) => {
     gridCells.forEach((cell) => {
-      cell.style.backgroundColor = "white";
+      cell.style.backgroundColor = "grey";
     });
   });
 };
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  renderGrid();
+  const initalGridSize = 8;
+  const selectGridSize = document.getElementById("grid-size");
+  selectGridSize.addEventListener("change", () => {
+    console.log(selectGridSize.value);
+    renderGrid(selectGridSize.value);
+  });
+  renderGrid(initalGridSize);
   renderCellHoverEffect();
   clearGrid();
 });
