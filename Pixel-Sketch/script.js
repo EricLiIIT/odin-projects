1b92c045dc9d504185f80756d58aa616564c7529
Eric <eli7@hawk.iit.edu>
2022-07-22 23:01:12 -0500

    clean: organize code

--- Pixel-Sketch/script.js
+++ Pixel-Sketch/script.js
@@ -1,8 +1,16 @@
-const renderGrid = (gridSize) => {
+const initalGridSize = 16;
+const pixelSketch = document.getElementById("pixel-sketch");
+const colorPicker = document.getElementById("color-input");
+const clearButton = document.getElementById("clear-button");
+const selectGridSize = document.getElementById("grid-size");
+const toggleButton = document.getElementById("toggle-button");
+
+let selectedColor = colorPicker.value;
+
+function renderGrid(gridSize) {
   removeLastGrid(); // Removes grid to make room for new grid
 
   // Add new grid
-  const pixelSketch = document.getElementById("pixel-sketch");
   const newGridContainer = document.createElement("div");
   newGridContainer.id = "grid-container";
   pixelSketch.appendChild(newGridContainer);
@@ -18,52 +26,58 @@ const renderGrid = (gridSize) => {
     let cell = document.createElement("div");
     cell.className = "grid-cell";
     cell.style.backgroundColor = "white";
+    cell.addEventListener("mouseover", fillCellColor);
+    cell.addEventListener("mousedown", fillCellColor);
     gridContainer.appendChild(cell);
   }
   // After changing grid size, this keeps the color selected
-  const colorPicker = document.getElementById("color-input");
   let selectedColor = colorPicker.value;
   renderCellHoverEffect(selectedColor);
-};
+}
 
-const clearGrid = (gridCells) => {
+function clearGrid(gridCells) {
   gridCells.forEach((cell) => {
     cell.style.backgroundColor = "white";
   });
-};
+}
 
-const renderCellHoverEffect = (selectedColor) => {
+function renderCellHoverEffect(selectedColor) {
+  let gridCells = document.querySelectorAll(".grid-cell");
+  gridCells.forEach((cell) => {
+    cell.addEventListener("mouseover", fillCellColor);
+    cell.addEventListener("mousedown", fillCellColor);
+  });
+}
+
+// ===== THIS IS BREAKING SLIDER ===== //
+
+function fillCellColor(event) {
+  // let mouseDown = false;
+  // document.body.onmousedown = () => (mouseDown = false);
+  // document.body.onmouseup = () => (mouseDown = true);
+  const colorPicker = document.getElementById("color-input");
+  let selectedColor = colorPicker.value;
   if (selectedColor == undefined) {
     selectedColor = "#000000";
   }
-  let gridCells = document.querySelectorAll(".grid-cell");
-  gridCells.forEach((cell) => {
-    cell.addEventListener("mouseover", (event) => {
-      if (event.buttons == 1) {
-        cell.style.backgroundColor = selectedColor;
-      }
-    });
-    cell.addEventListener("click", () => {
-      console.log("clicked");
-      cell.style.backgroundColor = selectedColor;
-    });
-  });
-};
+  if (event.type === "mouseover" && !mouseDown)
+    event.target.style.backgroundColor = selectedColor;
+}
 
-const removeLastGrid = () => {
+function removeLastGrid() {
   let grid = document.getElementById("grid-container");
   grid.remove();
-};
+}
 
-const loadGridLines = () => {
+function loadGridLines() {
   let gridCells = document.querySelectorAll(".grid-cell");
   gridCells.forEach((cell) => {
     cell.style.borderStyle = "solid";
     cell.style.borderWidth = "0.001em";
   });
-};
+}
 
-const toggleGrid = (gridCells) => {
+function toggleGrid(gridCells) {
   gridCells.forEach((cell) => {
     if (cell.style.borderStyle == "" || cell.style.borderStyle == "solid") {
       cell.style.border = "none";
@@ -72,14 +86,9 @@ const toggleGrid = (gridCells) => {
       cell.style.borderWidth = "0.001em";
     }
   });
-};
+}
 
-window.addEventListener("DOMContentLoaded", () => {
-  const initalGridSize = 16;
-
-  const colorPicker = document.getElementById("color-input");
-  let selectedColor = colorPicker.value;
-
+window.onload = () => {
   colorPicker.addEventListener("input", () => {
     selectedColor = colorPicker.value;
     console.log("Selected Color: ", selectedColor);
@@ -89,13 +98,11 @@ window.addEventListener("DOMContentLoaded", () => {
   renderGrid(initalGridSize);
   loadGridLines();
 
-  const clearButton = document.getElementById("clear-button");
   clearButton.addEventListener("click", () => {
     let gridCells = document.querySelectorAll(".grid-cell");
     clearGrid(gridCells);
   });
 
-  const selectGridSize = document.getElementById("grid-size");
   selectGridSize.addEventListener("input", () => {
     let gridSize = selectGridSize.value;
     document.getElementById("pixel-count").textContent = gridSize;
@@ -104,11 +111,12 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log("Grid Size: ", gridSize, "px ct");
   });
 
-  const toggleButton = document.getElementById("toggle-button");
   toggleButton.addEventListener("click", () => {
     let gridCells = document.querySelectorAll(".grid-cell");
     toggleGrid(gridCells);
   });
-});
+};
 
+// add label to color select
+// add manual number input to specify grid size
 // add a save to pc function somewhere (maybe need to use canvas?)
